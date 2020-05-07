697532f392fb0ac271bdadc6182da7e8b0515c8b
Ashlen Mencia <ashmencia0103@gmail.com>
2020-05-07 04:22:27 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+      if self.end_with?(".")
+        return true
+      end
+        return false
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    end
+      return false
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    end
+      return false
   end
 
   def count_sentences
-
+    # call on count_sentences method and get back a string that has the count
+    count_sentences.string.slice!(3) { |sentence| sentence == 3}
+    
   end
 end
